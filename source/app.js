03b940181030e4a4cfb23cce522a8644868a8573
kivyin <39168028+kivyin@users.noreply.github.com>
2022-01-29 11:43:40 -0500

    version 1.2
    Added view.js to all pages and revamped the nav

--- source/app.js
+++ source/app.js
@@ -1,1 +1,58 @@
 console.log("hello world");
+
+
+Vue.component('nav-item', {
+    props: ['nav'],
+    template: '<li class="nav-item"><a class="nav-link" :class="{nav.id==0 ? brandClass}" :href="nav.link">{{nav.text}}</a></li>'
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: '//Created by Kevin Twitty 6/10/2021 (function(){console.log("Hello World!");})();',
+        brandClass: 'navbar-brand',
+        name: "Kevin Twitty",
+        birthDate: "6/29/1976",
+        nav: [
+            { id: 0, link: 'index.html', text: 'Kevin Twitty' },
+            { id: 1, link: 'skills.html', text: 'Skills' },
+            { id: 2, link: 'exp.html', text: 'Experience' },
+            { id: 3, link: 'hobbies.html', text: 'Hobbies' },
+            { id: 4, link: 'notebook.html', text: 'Notebook' },
+            { id: 5, link: 'version.html', text: 'Version' }
+        ],
+        skills: [
+            { name: "HTML", rating: "8", url: "#HTML", shortDescription: "Used on all Service Portal projects" },
+            { name: "CSS", rating: "7" },
+            { name: "JavaScript", rating: "7.5" },
+            { name: "angular", rating: "" },
+            { name: "vuejs", rating: "" },
+            { name: "MSSql", rating: "" },
+            { name: ".NET 3.5", rating: "" },
+            { name: "ServiceNow", rating: "" },
+            { name: "PowerShell", rating: "" },
+            { name: "Windows Server 2019", rating: "" },
+            { name: "Ubuntu", rating: "" },
+            { name: "NodeJS", rating: "" },
+        ],
+        versionList: [
+            { date: "", number: "1.0", notes: "Initial Build -- Just Layout and some data." },
+            { date: "", number: "1.1", notes: "Adding Style -- Bootstrap and Hello World Javascript!" },
+            { date: "", number: "1.2", notes: "Starting Logic -- Added vuejs for Nav!" }
+        ]
+    },
+    methods: {
+        //Method used to add the Active flag if the current page matches the Page in the nav array.
+        isActivePage(page) {
+            var path = window.location.pathname;
+            var currentPage = path.split("/").pop();
+            //console.log(currentPage);
+            if (page == currentPage) {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+})
